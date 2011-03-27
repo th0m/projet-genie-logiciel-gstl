dbdeca04163c173d2949ff9ebebed698c72ffd8f
axelscht <axelscht@468702e1-5092-14fc-779a-dee4b53e0e13>
2011-03-27 19:43:56 +0000

    * Ajout du support de la gestion des differentes positions du vehicule

--- src/playercar.cpp
+++ src/playercar.cpp
@@ -1,12 +1,55 @@
 #include "playercar.hpp"
 #include "game.hpp"
+
+#include <SDL/SDL_Image.h>
 #include <list>
-#include "limit.hpp"
 
 
 PlayerCar::PlayerCar(Sint32 x, Sint32 y, SDL_Surface *window)
-: Shape(x, y, std::string("playercar"), window), m_shapeSize(Game::getShapeSize())
+: Shape(x, y, std::string("playercarg"), window), m_shapeSize(Game::getShapeSize())
 {
+    /* # On charge les differentes sprites */
+    m_up = IMG_Load("sprites/playercarh");
+    m_down = IMG_Load("sprites/playercarb");
+    m_right = IMG_Load("sprites/playercard");
+    m_left = m_img;
+
+    m_currentPos = Left;
+}
+
+
+PlayerCar::~PlayerCar()
+{
+    /* # On decharge proprement les images */
+    SDL_FreeSurface(m_up);
+    SDL_FreeSurface(m_down);
+    SDL_FreeSurface(m_right);
+}
+
+void PlayerCar::loadAnotherPosition(SDLKey key)
+{
+    /* # Ici on est charge de blitter le nouveau sprite de position selon la touche appuye */
+    switch(key)
+    {
+        case SDLK_LEFT :
+            m_img = m_left, m_currentPos = Left;
+        break;
+
+        case SDLK_RIGHT :
+            m_img = m_right, m_currentPos = Right;
+        break;
+
+        case SDLK_DOWN :
+            m_img = m_down, m_currentPos = Down;
+        break;
+
+        case SDLK_UP :
+            m_img = m_up, m_currentPos = Up;
+        break;
+
+        default :
+        break;
+    }
 }
 
 void PlayerCar::move(SDLKey key, std::list<Limit*> &limit)
@@ -18,27 +61,38 @@ void PlayerCar::move(SDLKey key, std::list<Limit*> &limit)
     {
         case SDLK_LEFT :
         {
-            x -= m_shapeSize;
+            if(m_currentPos == Left)
+                x -= m_shapeSize;
             break;
         }
 
         case SDLK_RIGHT :
         {
-            x += m_shapeSize;
+            if(m_currentPos == Right)
+                x += m_shapeSize;
             break;
         }
 
         case SDLK_UP :
         {
-            y -= m_shapeSize;
+            if(m_currentPos == Up)
+                y -= m_shapeSize;
             break;
         }
 
         case SDLK_DOWN :
         {
-            y += m_shapeSize;
+            if(m_currentPos == Down)
+                y += m_shapeSize;
             break;
         }
+    }
+
+    /* # Si les composantes sont egales ça veut dire qu'il faut que l'on charge un nouveau sprite de position */
+    if(x == m_x && y == m_y)
+    {
+        loadAnotherPosition(key);
+        return;
     }
 
     /* # On verifie que l'on va pas déplacer le véhicule dans une bordure */
--- src/playercar.hpp
+++ src/playercar.hpp
@@ -11,11 +11,29 @@ class PlayerCar : public Shape
 
         explicit PlayerCar(Sint32 x, Sint32 y, SDL_Surface *window);
 
+        ~PlayerCar();
+
+        /* # Fonction qui permet de deplacer le vehicule */
         void move(SDLKey key, std::list<Limit*> &limit);
 
     private:
 
+        enum CurrentPosition
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        };
+
+        void loadAnotherPosition(SDLKey key);
+
+        /* # Taille des sprites */
         Uint32 m_shapeSize;
+
+        SDL_Surface *m_up, *m_down, *m_right, *m_left;
+
+        CurrentPosition m_currentPos;
 };
 
 #endif
