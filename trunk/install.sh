41579ab51127fe841d566e830f7114c3d6b2f516
axelscht <axelscht@468702e1-5092-14fc-779a-dee4b53e0e13>
2011-04-15 23:04:05 +0000

    * GSTL FTW

--- trunk/install.sh
+++ trunk/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Script d'installation des dependances pour le projet génie logiciel INFO3 (pour distribution basée sur debian)
-#Par Goussen Maxime, Souchet Axel, Tawfeek Jehad, Lefebvre Thomas
+#GSTL
 
 #Installation des composants SDL de developpement
 sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev subversion
