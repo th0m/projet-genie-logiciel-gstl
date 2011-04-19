e6947d1bcf63cce13815f2c13683738e8b38f1b3
axelscht <axelscht@468702e1-5092-14fc-779a-dee4b53e0e13>
2011-04-19 20:17:26 +0000

    * commentaires doxygen sort/startingfinishline

--- trunk/src/sort.hpp
+++ trunk/src/sort.hpp
@@ -1,3 +1,14 @@
+/**
+ * \file sort.hpp
+ * \brief Classe Sort.
+ * \author GSTL
+ * \version 0.1
+ * \date 19 avril 2011
+ *
+ * Foncteur qui va réaliser le trie du conteneur de score.
+ *
+ */
+
 #ifndef SORT_HPP
 #define SORT_HPP
 
@@ -8,6 +19,12 @@ class Sort
 {
     public:
 
+        /**
+         * \fn bool operator()(std::pair<unsigned int, std::string> const &a, std::pair<unsigned int, std::string> const &b) const
+         * \brief Operateur() qui va permettre de trier deux pairs de scores/joueurs.
+         *
+         * \return Un booléen qui nous renseigne si l'objet a est plus petit que le b.
+         */
         bool operator()(std::pair<unsigned int, std::string> const &a, std::pair<unsigned int, std::string> const &b) const;
 };
 
