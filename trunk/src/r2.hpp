e43b9d9205cbf2e6c83f2bf169b26d9532d8da21
axelscht <axelscht@468702e1-5092-14fc-779a-dee4b53e0e13>
2011-04-19 19:48:10 +0000

    * Commentaire sur r1/r2/r3

--- trunk/src/r2.hpp
+++ trunk/src/r2.hpp
@@ -1,3 +1,14 @@
+/**
+ * \file r2.hpp
+ * \brief Gestion de la course 2.
+ * \author GSTL
+ * \version 0.1
+ * \date 19 avril 2011
+ *
+ * Classe qui va permettre de gérer la course 2 du jeux.
+ *
+ */
+
 #ifndef R2_HPP
 #define R2_HPP
 
@@ -7,11 +18,24 @@ class R2 : public Race
 {
     public:
 
+        /**
+         * \fn explicit R2(SDL_Surface *window)
+         * \brief Constructeur de la classe, avec initialisation de sa position grâce à x et y et passage d'un pointeur sur la fenetre du jeux (afin de s'y blitter).
+         *
+         * \param window Pointeur sur la fenetre principal.
+         */
         explicit R2(SDL_Surface *window);
 
+        /**
+         * \fn ~R2()
+         * \brief Destructeur de la classe, destruction de l'ensemble des ressources allouees.
+         */
         ~R2();
 
-        /* # Fonction qui va permettre de charger la course - celle-ci etant overide pour permettre de configurer les trajectoires des IAs specifiques à chaque course */
+        /**
+         * \fn void load()
+         * \brief Fonction qui va charger la course à l'ecran, initialiser les IAs et leurs trajectoires etc.
+         */
         void load();
 };
 
