82025560ece6699015c4fc59b818dddd67ad9df9
axelscht <axelscht@468702e1-5092-14fc-779a-dee4b53e0e13>
2011-04-19 20:13:17 +0000

    * Commentaires doxygen sand + score.cpp * Fix du top5 en top10

--- src/sand.hpp
+++ src/sand.hpp
@@ -1,3 +1,14 @@
+/**
+ * \file sand.hpp
+ * \brief Classe de l'élément "sable".
+ * \author GSTL
+ * \version 0.1
+ * \date 19 avril 2011
+ *
+ * Classe qui gère l'affichage d'une shape imitation sable.
+ *
+ */
+
 #ifndef SAND_HPP
 #define SAND_HPP
 
@@ -7,6 +18,14 @@ class Sand : public Shape
 {
     public:
 
+        /**
+         * \fn explicit Sand(float x, float y, SDL_Surface *window)
+         * \brief Constructeur de la classe, avec initialisation de sa position grâce à x et y et passage d'un pointeur sur la fenetre du jeux (afin de s'y blitter).
+         *
+         * \param x Coordonnée x du placement de la shape.
+         * \param y Coordonnée y du placement de la shape.
+         * \param window Pointeur sur la fenetre principal.
+         */
         explicit Sand(float x, float y, SDL_Surface *window);
 };
 
