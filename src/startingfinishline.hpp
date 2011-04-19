53e3726e090cd5e343fed5e152488cfd15793c53
axelscht <axelscht@468702e1-5092-14fc-779a-dee4b53e0e13>
2011-04-19 20:17:26 +0000

    * commentaires doxygen sort/startingfinishline

--- src/startingfinishline.hpp
+++ src/startingfinishline.hpp
@@ -1,3 +1,14 @@
+/**
+ * \file startingfinishline.hpp
+ * \brief Gestion de la ligne d'arrive/depart.
+ * \author GSTL
+ * \version 0.1
+ * \date 19 avril 2011
+ *
+ * Classe qui va permettre de gérer l'affichage de la ligne d'arrivé/départ.
+ *
+ */
+
 #ifndef STARTINGFINISHLINE_HPP
 #define STARTINGFINISHLINE_HPP
 
@@ -7,6 +18,14 @@ class StartingFinishLine : public Shape
 {
     public:
 
+        /**
+         * \fn explicit StartingFinishLine(float x, float y, SDL_Surface *window)
+         * \brief Constructeur de la classe, avec initialisation de sa position grâce à x et y et passage d'un pointeur sur la fenetre du jeux (afin de s'y blitter).
+         *
+         * \param x Coordonnée x du placement de la shape.
+         * \param y Coordonnée y du placement de la shape.
+         * \param window Pointeur sur la fenetre principal.
+         */
         explicit StartingFinishLine(float x, float y, SDL_Surface *window);
 };
 
