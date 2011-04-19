f164778b0d6ebff86c6514219dd4450d2cf550dc
axelscht <axelscht@468702e1-5092-14fc-779a-dee4b53e0e13>
2011-04-19 19:38:25 +0000

    * Commentaires doxygen de iarcar/limit/playercar

--- src/iacar.hpp
+++ src/iacar.hpp
@@ -1,3 +1,14 @@
+/**
+ * \file iacar.hpp
+ * \brief Gestion des IAs.
+ * \author GSTL
+ * \version 0.1
+ * \date 19 avril 2011
+ *
+ * Classe qui va permettre de gérer l'ensemble des pseudo-IAs.
+ *
+ */
+
 #ifndef IACAR_HPP
 #define IACAR_HPP
 
@@ -9,44 +20,84 @@ class IACar : public Shape
 {
     public:
 
+        /**
+         * \enum Step
+         * \brief Nos IAs sont très basiques, en effet elles suivent quatres etats.
+         *
+         */
         enum Step
         {
-            FirstStep,
-            SecondStep,
-            ThirdStep,
-            LastStep
+            FirstStep, /*!< Première état, de gauche à droite (en partant de la ligne d'arrive) */
+            SecondStep, /*!< Second état, de bas en haut */
+            ThirdStep, /*!< Troisieme état, de droite à gauche */
+            LastStep /*!< Dernière état de haut en bas */
         };
 
+        /**
+         * \fn explicit IACar(float x, float y, SDL_Surface *window)
+         * \brief Constructeur de la classe, avec initialisation de sa position grâce à x et y et passage d'un pointeur sur la fenetre du jeux (afin de s'y blitter).
+         *
+         * \param x Coordonnée x du placement de la shape.
+         * \param y Coordonnée y du placement de la shape.
+         * \param window Pointeur sur la fenetre principal.
+         */
         explicit IACar(float x, float y, SDL_Surface *window);
 
+        /**
+         * \fn ~IACar()
+         * \brief Destructeur de la classe, destruction de l'ensemble des ressources allouees.
+         */
         ~IACar();
 
-        /* # Fonction qui permet de définir les points "d'interpolation" du mouvement */
+        /**
+         * \fn void setPoints(std::vector<float> &points)
+         * \brief Fonction qui permet de définir les points "d'interpolation" du mouvement.
+         *
+         * \param points Un vecteur qui va contenir 4 coordonnée, une coordonnée x en premier, une coordonnée y en second, une coordonnée x en troisième et enfin une autre coordonnée y.
+         */
         void setPoints(std::vector<float> &points);
 
-        /* # Fonction qui permet de faire evoluer les voitures pseudo-IAs */
+        /**
+         * \fn void move(float playerx, float playery, std::list<IACar*> &ias)
+         * \brief Fonction qui permet de faire evoluer les voitures pseudo-IAs.
+         *
+         * \param playerx Coordonnée x de la voiture du joueur (pour éviter les collisions).
+         * \param playery Coordonnée y de la voiture du joueur (pour éviter les collisions).
+         * \param ias Liste des différentes IAs encore une fois pour éviter les collisions.
+         */
         void move(float playerx, float playery, std::list<IACar*> &ias);
 
-        /* # Fonction qui permet de définir la difficulte d'une IA */
+        /**
+         * \fn void setDifficulty(Uint8 diff)
+         * \brief Fonction qui permet de définir la difficulte d'une IA à partir d'un entier de 0 à 3, il va permettre de "muter" leurs vitesses/mouvements etc.
+         *
+         * \param diff Difficulté de 0 à 3 de l'IA.
+         */
         void setDifficulty(Uint8 diff);
 
     private:
 
-        /* # Fonction qui est responsable qui va autoriser ou non l'IA a avance */
-        bool moveAllowed(float x, float y, float playerx, float playery,  std::list<IACar*> &ias);
+        /**
+         * \fn bool moveAllowed(float x, float y, float playerx, float playery, std::list<IACar*> &ias)
+         * \brief Fonction qui permet de définir la difficulte d'une IA à partir d'un entier de 0 à 3, il va permettre de "muter" leurs vitesses/mouvements etc.
+         *
+         * \param x Coordonnée x de l'IA.
+         * \param y Coordonnée y de l'IA.
+         * \param playerx Coordonnée x de la voiture du joueur pour eviter les collisions.
+         * \param playery Coordonnée y de la voiture du joueur pour eviter les collisions.
+         * \param ias Liste des différentes IAs encore une fois pour éviter les collisions.
+         * \return Un booléen pour savoir si le mouvement de l'IA va entrainer une collision ou non.
+         */
+        bool moveAllowed(float x, float y, float playerx, float playery, std::list<IACar*> &ias);
 
 
-        /* # Vecteur des points d'interpolations */
-        std::vector<float> m_points;
+        std::vector<float> m_points; /*!< Vecteur des points d'interpolations */
 
-        /* # Le mouvement s'oppère en 4 étapes, cet attribut indique le courant */
-        Step m_currentStep;
+        Step m_currentStep; /*!< Le mouvement s'oppère en 4 étapes, cet attribut indique le courant */
 
-        /* # Difficulte de l'IA */
-        Uint8 m_difficulty;
+        Uint8 m_difficulty; /*!< Difficulte de l'IA */
 
-        /* # Les différentes surfaces de la voiture */
-        SDL_Surface *m_up, *m_down, *m_right, *m_left;
+        SDL_Surface *m_up, *m_down, *m_right, *m_left; /*!< Les différentes surfaces de la voiture, voiture vers le haut, bas, gauche, droite */
 };
 
 #endif
--- src/limit.hpp
+++ src/limit.hpp
@@ -1,3 +1,14 @@
+/**
+ * \file limit.hpp
+ * \brief Classe de l'élément qui détermine une limite sur le circuit.
+ * \author GSTL
+ * \version 0.1
+ * \date 19 avril 2011
+ *
+ * Classe qui gère l'affichage d'une limite sur la carte.
+ *
+ */
+
 #ifndef LIMIT_HPP
 #define LIMIT_HPP
 
@@ -7,6 +18,14 @@ class Limit : public Shape
 {
     public:
 
+        /**
+         * \fn explicit Limit(float x, float y, SDL_Surface *window)
+         * \brief Constructeur de la classe, avec initialisation de sa position grâce à x et y et passage d'un pointeur sur la fenetre du jeux (afin de s'y blitter).
+         *
+         * \param x Coordonnée x du placement de la shape.
+         * \param y Coordonnée y du placement de la shape.
+         * \param window Pointeur sur la fenetre principal.
+         */
         explicit Limit(float x, float y, SDL_Surface *window);
 };
 
